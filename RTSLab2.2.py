596956a1a6bf00d3483784c72bd528ce8e1c8ee8
WhatIsLove007 <59250213+WhatIsLove007@users.noreply.github.com>
2020-06-02 16:20:00 +0300

    ADDITIONAL TASK

--- RTSLab2.2.py
+++ RTSLab2.2.py
@@ -1,56 +1,106 @@
-# Лабораторна робота №2.2
-# Нагорного В.В., групи ІВ-73
-# Варіант №22
-# Число гармонік в сигналі n = 10
-# Гранична частота, w_max = 1200
-# Кількість дискретних відліків, N = 64
-
-import random as r
-import math
 import matplotlib.pyplot as plt
+import math
+from random import *
+import numpy as np
+from multiprocessing import Pool
 
-n = 10
-w_max = 1200
-N = 64
-
-
-x = [0] * N
-
-for i in range(n):
-    A = r.randrange(2)
-    W = r.randrange(w_max)
-    f = r.randrange(1000000)
-    for t in range(N):
-        x[t] += A * math.sin(W * t + f)
+counts = 256
+frequency = 1500
+harmonics = 10
 
 
-def fast_fourier_trans(x: list):
-    N = len(x)
-    fft = [[0] * 2 for i in range(N)]
-    for i in range(N // 2):
-        array1 = [0] * 2
-        array2 = [0] * 2
-        for j in range(N // 2):
-            cos = math.cos(4 * math.pi * i * j / N)
-            sin = math.sin(4 * math.pi * i * j / N)
-            array1[0] += x[2 * j + 1] * cos # real
-            array1[1] += x[2 * j + 1] * sin # imag
-            array2[0] += x[2 * j] * cos # real
-            array2[1] += x[2 * j] * sin # imag
-        cos = math.cos(2 * math.pi * i / N)
-        sin = math.sin(2 * math.pi * i / N)
-        fft[i][0] = array2[0] + array1[0] * cos - array1[1] * sin # real
-        fft[i][1] = array2[1] + array1[0] * sin + array1[1] * cos # imag
-        fft[i + N // 2][0] = array2[0] - (array1[0] * cos - array1[1] * sin) # real
-        fft[i + N // 2][1] = array2[1] - (array1[0] * sin + array1[1] * cos) # imag
-    return fft
+
+def freg_gen(frequency, harmonics):
+    Freg = []
+    step = frequency / harmonics
+    for i in range(harmonics):
+        Freg.append(frequency - step * i)
+    return Freg
 
 
-fft = fast_fourier_trans(x)
+def expectation(x, counts):
+    mx = 0.0
+    for i in range(counts):
+        mx += x[i]
+    return mx / counts
 
-data_fft = [math.sqrt(fft[i][0] ** 2 + fft[i][1] ** 2) for i in range(N)]
 
-plt.plot([i for i in range(N)], x)
-plt.show()
-plt.plot([i for i in range(N)], data_fft)
-plt.show()
+def dispersion(x, counts, mx):
+    dx = 0.0
+    for i in range(counts):
+        dx += math.pow(x[i] - mx, 2)
+    return dx / (counts - 1)
+
+
+def xt_gen(counts, harmonics, Arraray_new, Freg, alpha):
+    x = [0] * counts
+    for j in range(counts):
+        for i in range(harmonics):
+            x[j] += Arraray_new[i] * math.sin(Freg[i] * j + alpha[i])
+    return x
+
+
+def arr_gen(harmonics, min, max):
+    arr = [0] * harmonics
+    for i in range(harmonics):
+        arr[i] = randint(min, max)
+    return arr
+
+
+def dpf(signal):
+    harmonics = len(signal)
+    p = np.arange(harmonics)
+    k = p.reshape((harmonics, 1))
+    frequency = np.exp(-2j * np.pi * p * k / harmonics)
+    return np.dot(frequency, signal)
+
+
+def fft(signal):
+    signal = np.asanyarray(signal, dtype=float)
+    counts = len(signal)
+    if counts <= 2:
+        return dpf(signal)
+    else:
+        signal_even = fft(signal[::2])
+        signal_odd = fft(signal[1::2])
+        terms = np.exp(-2j * np.pi * np.arange(counts) / counts)
+        return np.concatenate([signal_even + terms[:counts // 2] * signal_odd,
+                               signal_even + terms[counts // 2:] * signal_odd])
+
+def fft_parallel(signal):
+    p = Pool(2)
+    signal = np.asanyarray(signal, dtype=float)
+    counts = len(signal)
+    if counts <= 2:
+        return dpf(signal)
+    else:
+        signal_even_odd = (signal[::2], signal[1::2])
+        signal_even, signal_odd = p.map(fft, signal_even_odd)
+        terms = np.exp(-2j * np.pi * np.arange(counts) / counts)
+        return np.concatenate([signal_even + terms[:counts // 2] * signal_odd,
+                               signal_even + terms[counts // 2:] * signal_odd])
+
+
+t = np.linspace(0, 10, counts)
+x_dpf_real = x_dpf.real
+x_dpf_img = x_dpf.imag
+x_fft_real = x_fft.real
+x_fft_img = x_fft.imag
+
+
+def draw_dpf():
+    plt.title("ДИСКРЕТНОГО ПЕРЕТВОРЕННЯ ФУР'Є")
+    plt.plot(t, x_dpf_real, 'b', t, x_dpf_img, 'r')
+    plt.show()
+
+
+def draw_fft(x_fft_real, x_fft_img):
+    plt.title("ШВИДКЕ ПЕРЕТВОРЕННЯ ФУР'Є З ЧАСОМ")
+    plt.plot(t, x_fft_real, 'b', t, x_fft_img, 'r')
+    plt.show()
+
+x_fft = fft_parallel(x)
+x_fft_real2 = x_fft.real
+x_fft_img2 = x_fft.imag
+
+draw_fft(x_fft_real2, x_fft_img2)
